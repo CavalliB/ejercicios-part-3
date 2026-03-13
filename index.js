0b3775bf297e95f6905713eb59d9b1a0cbebd7e9
CavalliB <Cerdopigman12@hotmail.com>
2026-03-13 01:12:44 -0300

    3.2

--- index.js
+++ index.js
@@ -40,6 +40,12 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+  const date = new Date()
+  const info = `Phonebook has info for ${persons.length} people <br> ${date}`
+  response.send(info)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
